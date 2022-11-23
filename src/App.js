0f13576522728a6e429a3644f82acf6356b0806f
German Caicedo <german.caicedoa1@gmail.com>
2022-11-23 15:12:07 -0500

    ItemListContainer was added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Main from "./components/Main";
 import Shop from "./components/Shop";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import ItemListContainer from "./components/ItemListContainer";
 
 function App() {
   return (
     <div>
       <Header />
+      <ItemListContainer greeting= {"Welcome to German Caicedo Proyect"} />
       <Main />
       <Shop />
       <Contact />
