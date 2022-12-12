ad652015a0b8560bc962a99d8eb527155d4e2f5b
German Caicedo <german.caicedoa1@gmail.com>
2022-12-12 18:42:25 -0500

    Correcion de bugs

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,9 +3,13 @@ import Item from "./Item";
 
 const ItemList = ({ items }) => {
     return (
-        <div>
+        <div className="row">
             {
-                items.map(item => <Item key={item.id} item={item} />)
+                items.map(item =>
+                    <div className="col-md-4">
+                        <Item key={item.id} item={item} />
+                   </div>
+                )
             }
         </div>)
 }
